3b0dfd23b933508967d7093e75c51d607202252a
ashbbdu <ashishsrivastava7007@gmail.com>
2026-07-28 23:53:33 +0530

    pattern7 done

--- dsa/Basics/Patterns/Pattern7.java
+++ dsa/Basics/Patterns/Pattern7.java
@@ -1,4 +1,20 @@
 package dsa.Basics.Patterns;
 
 public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int row = 0 ; row < n ; row++) {
+//            spaces
+            for(int col = 0 ;  col < n - row - 1 ; col++) {
+                System.out.print(" ");
+            }
+
+//            stars
+            for(int col = 0 ; col < 2*row+1 ; col++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
