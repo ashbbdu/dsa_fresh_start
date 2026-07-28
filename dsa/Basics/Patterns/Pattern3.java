b92a258be54b34f64dbb6bf2624181f2d4011a6a
ashbbdu <ashishsrivastava7007@gmail.com>
2026-07-28 23:19:18 +0530

    pattern3 done

---
+++ dsa/Basics/Patterns/Pattern3.java
@@ -0,0 +1,13 @@
+package dsa.Basics.Patterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int row = 0 ; row < n ; row++) {
+            for(int col = 0 ; col < row + 1 ; col++) {
+                System.out.print(col+1);
+            }
+            System.out.println();
+        }
+    }
+}
